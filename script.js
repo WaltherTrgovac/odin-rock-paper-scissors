fd283ee68dd7a1811f1d45510905d428de5f3b87
Walther <razi.black2014@hotmail.com>
2025-04-23 00:12:12 +0200

    game without restart

--- script.js
+++ script.js
@@ -1,11 +1,107 @@
-function playGame() {
+function playGame(){
 
     let humanScore = 0;
     let computerScore = 0;
 
+    function decodeHtmlEntity(html) {
+        const txt = document.createElement("textarea");
+        txt.innerHTML = html;
+        return txt.value;
+    }
+
+    function changeUI(computerChoice, humanChoice, computerScore, humanScore, computerText, humanText){
+        // Change Images
+        let computerImg = document.querySelector('.chosen-img-computer');
+        let playerImg = document.querySelector('.chosen-img-player');
+
+        switch (computerChoice){
+            case "rock":
+                computerImg.textContent = decodeHtmlEntity('&#x1F44A');
+                break;
+            case "paper":
+                computerImg.textContent = decodeHtmlEntity('&#x1F590');
+                break;
+            case "scissors":
+                computerImg.textContent = decodeHtmlEntity('&#x270C');
+                break;
+        }
+
+        switch (humanChoice){
+            case "rock":
+                playerImg.textContent = decodeHtmlEntity('&#x1F44A');
+                break;
+            case "paper":
+                playerImg.textContent = decodeHtmlEntity('&#x1F590');
+                break;
+            case "scissors":
+                playerImg.textContent = decodeHtmlEntity('&#x270C');
+                break;
+        }
+
+        // Change score
+        let computerScoreEl = document.querySelector('#computer-score');
+        let humanScoreEl = document.querySelector('#player-score');
+
+        computerScoreEl.textContent = computerScore;
+        humanScoreEl.textContent = humanScore;
+
+
+        // Change text
+        let computerTextEl = document.querySelector('.information-text');
+        let humanTextEl = document.querySelector('.main-text');
+
+        computerTextEl.textContent = `Computer chose ${computerText}!`;
+        humanTextEl.textContent = `You chose ${humanText}!`;
+    }
+
+    // There is a better way to write this, but I don't want to introduce anything new
+    // that has not been yet introduced in the course.
+    function playRound(computerChoice, humanChoice) {
+        let computerChoiceLower = computerChoice.toLowerCase();
+        let humanChoiceLower = humanChoice.toLowerCase();
+        if (humanChoiceLower === "rock" && computerChoiceLower === "scissors"){
+            humanScore += 1;
+        }
+
+        if (humanChoiceLower === "rock" && computerChoiceLower === "rock"){
+            humanScore += 1;
+            computerScore += 1;
+        }
+
+        if (humanChoiceLower === "rock" && computerChoiceLower === "paper"){
+            computerScore += 1;
+        }
+
+        if (humanChoiceLower === "paper" && computerChoiceLower === "rock"){
+            humanScore += 1;
+        }
+
+        if (humanChoiceLower === "paper" && computerChoiceLower === "paper"){
+            humanScore += 1;
+            computerScore += 1;
+        }
+
+        if (humanChoiceLower === "paper" && computerChoiceLower === "scissors"){
+            computerScore += 1;
+        }
+
+        if (humanChoiceLower === "scissors" && computerChoiceLower === "paper"){
+            humanScore += 1;
+        }
+
+        if (humanChoiceLower === "scissors" && computerChoiceLower === "scissors"){
+            humanScore += 1;
+            computerScore += 1;
+        }
+
+        if (humanChoiceLower === "scissors" && computerChoiceLower === "rock"){
+            computerScore += 1;
+        }
+        changeUI(computerChoiceLower, humanChoiceLower, computerScore, humanScore, computerChoiceLower, humanChoiceLower);
+    }
+
     function getComputerChoice() {
         let randomChoice = Math.floor(Math.random() * 3);
-
         switch (randomChoice){
             case 0:
                 return "rock";
@@ -17,85 +113,41 @@ function playGame() {
     }
 
     function getHumanChoice() {
-        let humanChoice = prompt("Pick between rock, paper or scissors.")
+        const chosenWeapon = document.querySelector('.options-container');
 
-        if (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors"){
-            console.log("Your input is incorrect.")
-        } else {
-            return humanChoice;
-        }
-    }
-
-    // There is a better way to write this, but I don't want to introduce anything new
-    // that has not been yet introduced in the course.
-    function playRound(humanChoice, computerChoice) {
-        let humanChoiceLower = humanChoice.toLowerCase();
-        let computerChoiceLower = computerChoice.toLowerCase();
-        if (humanChoiceLower === "rock" && computerChoiceLower === "scissors"){
-            console.log("You win! Rock beats scissors.");
-            humanScore += 1;
-        }
-
-        if (humanChoiceLower === "rock" && computerChoiceLower === "rock"){
-            console.log("It's a tie! You both chose rock.");
-            humanScore += 1;
-            computerScore += 1;
-        }
-
-        if (humanChoiceLower === "rock" && computerChoiceLower === "paper"){
-            console.log("You lose! Paper beats rock.");
-            computerScore += 1;
-        }
-
-        if (humanChoiceLower === "paper" && computerChoiceLower === "rock"){
-            console.log("You win! Paper beats rock.");
-            humanScore += 1;
-        }
-
-        if (humanChoiceLower === "paper" && computerChoiceLower === "paper"){
-            console.log("It's a tie! You both chose paper.");
-            humanScore += 1;
-            computerScore += 1;
-        }
-
-        if (humanChoiceLower === "paper" && computerChoiceLower === "scissors"){
-            console.log("You lose! Scissors beats paper.");
-            computerScore += 1;
-        }
-
-        if (humanChoiceLower === "scissors" && computerChoiceLower === "paper"){
-            console.log("You win! Scissors beats paper.");
-            humanScore += 1;
-        }
-
-        if (humanChoiceLower === "scissors" && computerChoiceLower === "scissors"){
-            console.log("It's a tie! You both chose scissors.");
-            humanScore += 1;
-            computerScore += 1;
-        }
-
-        if (humanChoiceLower === "scissors" && computerChoiceLower === "rock"){
-            console.log("You lose! Rock beats scissors.");
-            computerScore += 1;
-        }
-    }
-
-    alert("Welcome to the game of rock paper scissors. You will be playing 5 rounds. Good luck!")
-    for (let i = 1; i <= 5; i++){
-        let humanChoice = getHumanChoice();
-        let computerChoice = getComputerChoice();
-        playRound(humanChoice, computerChoice);
-        console.log(`SCORE: You: ${humanScore}, Computer: ${computerScore}`)
-        if (i === 5){
-            if (humanScore > computerScore){
-                console.log("Congratulations! You won!")
-            } else if (humanScore < computerScore){
-                console.log("Computer won!")
-            } else {
-                console.log("The game ended with a tie.")
+        chosenWeapon.addEventListener('click', (e) => {
+            switch (e.target){
+                case document.querySelector('#rock'):
+                    playRound(getComputerChoice(), "rock");
+                    break;
+                case document.querySelector('#paper'):
+                    playRound(getComputerChoice(), "paper");
+                    break;
+                case document.querySelector('#scissors'):
+                    playRound(getComputerChoice(), "scissors");
+                    break;
             }
-        }
+
+            let informationText = document.querySelector('.information-text');
+            let mainText = document.querySelector('.main-text');
+
+            if (humanScore == 5 && computerScore == 5){
+                mainText.textContent = "It's a tie!"
+                informationText.textContent = '';
+            }
+            if (humanScore == 5){
+                mainText.textContent = "Congratulations, you won!"
+                informationText.textContent = '';
+            }
+            if (computerScore == 5){
+                mainText.textContent = "Computer won this time."
+                informationText.textContent = '';
+            }
+        })
     }
+
+
+    getHumanChoice();
 }
 
 playGame();
