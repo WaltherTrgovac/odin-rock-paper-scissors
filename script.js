0a6b13f44d550ab62802a994aaa055d02ef46c5c
Walther <razi.black2014@hotmail.com>
2025-04-24 22:37:48 +0200

    added styling and reset button

--- script.js
+++ script.js
@@ -54,6 +54,35 @@ function playGame(){
         humanTextEl.textContent = `You chose ${humanText}!`;
     }
 
+    function drawPlayAgainButton(){
+        let optionsContainer = document.querySelector('.options-container');
+        optionsContainer.innerHTML = '<button class="play-again-button">Play Again</button>';
+    }
+
+    function resetGame() {
+        let playAgainButton = document.querySelector('.play-again-button');
+
+        playAgainButton.addEventListener('click', () => {
+            let optionsContainer = document.querySelector('.options-container');
+            let computerScoreEl = document.querySelector('#computer-score');
+            let humanScoreEl = document.querySelector('#player-score');
+            let computerTextEl = document.querySelector('.information-text');
+            let humanTextEl = document.querySelector('.main-text');
+            let computerImg = document.querySelector('.chosen-img-computer');
+            let playerImg = document.querySelector('.chosen-img-player');
+
+            optionsContainer.innerHTML = '<button id="rock">&#x1F44A</button><button id="paper">&#x1F590</button><button id="scissors">&#x270C</button>'
+            humanScore = 0;
+            computerScore = 0;
+            computerScoreEl.textContent = computerScore;
+            humanScoreEl.textContent = humanScore;
+            computerTextEl.textContent = `Choose your option`;
+            humanTextEl.textContent = `First to 5 wins!`;
+            computerImg.textContent = decodeHtmlEntity('&#x2753');
+            playerImg.textContent = decodeHtmlEntity('&#x2753');
+        })
+    }
+
     // There is a better way to write this, but I don't want to introduce anything new
     // that has not been yet introduced in the course.
     function playRound(computerChoice, humanChoice) {
@@ -134,18 +163,23 @@ function playGame(){
             if (humanScore == 5 && computerScore == 5){
                 mainText.textContent = "It's a tie!"
                 informationText.textContent = '';
+                drawPlayAgainButton();
+                resetGame();
             }
             if (humanScore == 5){
                 mainText.textContent = "Congratulations, you won!"
                 informationText.textContent = '';
+                drawPlayAgainButton();
+                resetGame();
             }
             if (computerScore == 5){
                 mainText.textContent = "Computer won this time."
                 informationText.textContent = '';
+                drawPlayAgainButton();
+                resetGame();
             }
         })
     }
-
 
     getHumanChoice();
 }
